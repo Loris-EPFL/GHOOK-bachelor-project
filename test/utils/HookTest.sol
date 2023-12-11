aa31727f155e519ccdc4e0dc4457310296241c81
ltran <loris.tran@epfl.ch>
2023-12-11 20:25:41 +0100

    added test

--- test/utils/HookTest.sol
+++ test/utils/HookTest.sol
@@ -9,27 +9,35 @@ import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
 import {PoolModifyPositionTest} from "@uniswap/v4-core/contracts/test/PoolModifyPositionTest.sol";
 import {PoolSwapTest} from "@uniswap/v4-core/contracts/test/PoolSwapTest.sol";
 import {PoolDonateTest} from "@uniswap/v4-core/contracts/test/PoolDonateTest.sol";
+import {MockERC20} from "@uniswap/v4-core/test/foundry-tests/utils/MockERC20.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { console2 } from "forge-std/console2.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
 
 import {TestERC20} from "@uniswap/v4-core/contracts/test/TestERC20.sol";
 import {TickMath} from "@uniswap/v4-core/contracts/libraries/TickMath.sol";
 
 /// @notice Contract to initialize some test helpers
 /// @dev Minimal initialization. Inheriting contract should set up pools and provision liquidity
-contract HookTest is Test {
+contract HookTest is Test, PRBTest, StdCheats {
     PoolManager manager;
     PoolModifyPositionTest modifyPositionRouter;
     PoolSwapTest swapRouter;
     PoolDonateTest donateRouter;
-    TestERC20 token0;
-    TestERC20 token1;
+    MockERC20 token0;
+    MockERC20 token1;
+    address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     uint160 public constant MIN_PRICE_LIMIT = TickMath.MIN_SQRT_RATIO + 1;
     uint160 public constant MAX_PRICE_LIMIT = TickMath.MAX_SQRT_RATIO - 1;
 
     function initHookTestEnv() public {
         uint256 amount = 2 ** 128;
-        TestERC20 _tokenA = new TestERC20(amount);
-        TestERC20 _tokenB = new TestERC20(amount);
+        MockERC20 _tokenA = MockERC20(WETH);
+        MockERC20 _tokenB = MockERC20(USDC);
+
+        _mintTokens(amount);
 
         // pools alphabetically sort tokens by address
         // so align `token0` with `pool.token0` for consistency
@@ -67,5 +75,16 @@ contract HookTest is Test {
             PoolSwapTest.TestSettings({withdrawTokens: true, settleUsingTransfer: true});
 
         swapRouter.swap(key, params, testSettings, hookData);
+    }
+
+    //helper function to mint tokens
+    function _mintTokens(uint256 amount) internal{
+       
+ 
+        //mint Aeth and Ausdc by depositing into pool
+        deal(WETH, msg.sender, amount);
+        deal(USDC, msg.sender, amount);
+
+    
     }
 }
